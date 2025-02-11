035930da552cda1eff443b863a6f28bc9841c6f2
hemanth62892 <r.hemanthraju1998@gmail.com>
2025-02-11 18:54:16 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,20 @@ function goToBalloons() {
 }
 
 function showMessage() {
+    generateBalloons();
     setTimeout(() => {
         window.location.href = "/wishes";
     }, 5000);
+}
+
+function generateBalloons() {
+    let container = document.getElementById("balloon-container");
+    for (let i = 0; i < 20; i++) {
+        let balloon = document.createElement("div");
+        balloon.classList.add("balloon");
+        balloon.style.left = Math.random() * 100 + "vw";
+        balloon.style.animationDuration = (Math.random() * 3 + 2) + "s";
+        balloon.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 70%)`; // Random colors
+        container.appendChild(balloon);
+    }
 }
