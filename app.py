fb0ed542bc6362e37a7848f168b32b7edd9f315f
hemanth62892 <r.hemanthraju1998@gmail.com>
2025-02-11 18:17:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/balloons')
+def balloons():
+    return render_template('balloons.html')
+
+@app.route('/wishes')
+def wishes():
+    return render_template('wishes.html')
+
+@app.route('/quotes')
+def quotes():
+    return render_template('quotes.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
