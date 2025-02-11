330c074dbec3dd6dc18472f9ddddda0470402ea3
hemanth62892 <r.hemanthraju1998@gmail.com>
2025-02-11 18:25:19 +0530

    Create script.js

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+function goToBalloons() {
+    window.location.href = "/balloons";
+}
+
+function showMessage() {
+    setTimeout(() => {
+        window.location.href = "/wishes";
+    }, 5000);
+}
